9ce713c0c9c75be6b728bd98630f765f34bda2fc
maatheusgouveia <matheus_guitarplayer@hotmail.com>
2020-10-14 00:09:59 -0300

    Listar e criar registro de orfanato

---
+++ src/routes.ts
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import { getRepository } from "typeorm";
+import OrphanagesController from "./controllers/OrphanagesController";
+import Orphanage from "./models/Orphanage";
+
+const routes = Router();
+
+routes.get("/orphanages", OrphanagesController.index);
+routes.get("/orphanages/:id", OrphanagesController.show);
+routes.post("/orphanages", OrphanagesController.create);
+
+export default routes;
