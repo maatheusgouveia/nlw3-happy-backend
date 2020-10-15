5e0c6f6fd01ed0f16582e3895029857140338b02
maatheusgouveia <matheus_guitarplayer@hotmail.com>
2020-10-15 03:10:00 -0300

    views de retorno dos dados

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,7 @@
 import express from "express";
+import path from "path";
+import "express-async-errors";
+
 import "./database/connection";
 
 import routes from "./routes";
@@ -7,5 +10,6 @@ const app = express();
 
 app.use(express.json());
 app.use(routes);
+app.use("/uploads", express.static(path.join(__dirname, "..", "tmp")));
 
 app.listen(3333);
